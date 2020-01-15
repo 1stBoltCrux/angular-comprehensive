e6b6c1d649ebc914ffc92c0d55aa72d42f7802fb
Ted Leary <ted.e.leary@ey.com>
2020-01-14 16:02:21 -0800

    finish auth interceptor

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -3,9 +3,10 @@ import { Injectable } from "@angular/core";
 import {
   HttpInterceptor,
   HttpRequest,
-  HttpHandler
+  HttpHandler,
+  HttpParams
 } from "@angular/common/http";
-import { take } from "rxjs/operators";
+import { take, exhaustMap } from "rxjs/operators";
 import { User } from "./user.model";
 
 @Injectable()
@@ -13,18 +14,30 @@ export class AuthInterceptorService implements HttpInterceptor {
   user: User;
   constructor(private authService: AuthService) {}
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    this.authService.user.pipe(take(1)).subscribe(user => {
-      this.user = user;
-    });
-    if (this.user && this.user.token) {
-      const modifiedReq = req.clone({
-        params: req.params.append("auth", this.user.token)
-      });
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap(user => {
+        if (!user) {
+          return next.handle(req);
+        }
+        const modifiedReq = req.clone({
+          params: new HttpParams().set("auth", user.token)
+        });
+        return next.handle(modifiedReq);
+      })
+    );
+    // this.authService.user.pipe(take(1)).subscribe(user => {
+    //   this.user = user;
+    // });
+    // if (this.user && this.user.token) {
+    //   const modifiedReq = req.clone({
+    //     params: req.params.append("auth", this.user.token)
+    //   });
 
-      return next.handle(modifiedReq);
-    } else {
-      console.log("token does not exist" + req);
-      return next.handle(req);
-    }
+    //   return next.handle(modifiedReq);
+    // } else {
+    //   console.log("token does not exist" + req);
+    //   return next.handle(req);
+    // }
   }
 }
